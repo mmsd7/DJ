0afad26fc8e2a825bd66b31fb50d5bb00d9771e3
mmsd7 <yousuf.py7@gmail.com>
2024-05-05 21:04:12 +0600

    till 18

---
+++ trydjango/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article 
+import random
+
+def home_view(request):
+    random_id = random.randint(1, 4)
+    article_obj = Article.objects.get(id=random_id)
+    article_query_set = Article.objects.all()
+    context = {
+        'object_list' :article_query_set,
+        'title' : article_obj.title,
+        'content' : article_obj.content,
+        'id': article_obj.id,
+        'object': article_obj,
+    }
+    HTML_STRING = render_to_string('home_view.html', context= context)
+    return HttpResponse(HTML_STRING)

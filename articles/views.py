c0c547af53cc1fae2d7c6c8c12283a054696041d
mmsd7 <yousuf.py7@gmail.com>
2024-05-07 20:52:52 +0600

    22 - Search Form & Request Data

--- articles/views.py
+++ articles/views.py
@@ -7,3 +7,17 @@ def article_detail_view(request, id=None):
         article_obj = Article.objects.get(id=id)
     context = {'object': article_obj}
     return render(request, 'articles/detail.html', context=context)
+
+def article_search_view(request):
+    query_dict = request.GET
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+        
+    context = {'object': article_obj}
+    return render(request, 'articles/search.html', context=context)
